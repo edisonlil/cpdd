f44999510a8f112fcc9389cfecf9edae13ee2cf9
edison <edisonlil@163.com>
2023-12-27 16:32:44 +0800

    新增命令行解析

---
+++ README.md
@@ -0,0 +1,22 @@
+# CPDD - Copy Deep Designate
+
+Copy Deep Designate 是项目深度克隆工具，用于从源项目工程进行深度复制，并自动为新克隆的项目分配新的项目代号和权限设置。
+
+> "Copy"：复制源项目或工程。"Deep"：进行深度复制，包括所有相关的文件和目录结构。"Designate"：指定或赋予新的项目代号或权限。
+
+
+## 功能特性
+* **深度复制**：全面复制源项目的所有文件和目录结构，确保新项目与源项目完全一致。
+* **项目代号指定**：在复制完成后，自动为新项目分配一个新的项目代号，便于管理和区分不同的项目版本。
+
+## 环境依赖
+![img.png](img.png)
+
+## 安装
+//TODO
+## 使用示例
+
+```shell
+cpdd -d 'targetChars:sourceChars,targetChars:sourceChars' -o outputPath  <项目路径>
+```
+* `-h --help`：查看帮助命令
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 import shutil
 import chardet
+
 def detect_encoding(file_path):
     with open(file_path, 'rb') as f:
         result = chardet.detect(f.read())
@@ -36,11 +38,43 @@ def rename_and_replace_in_file(source_directory, target_directory, char_map):
             # 将修改后的内容写入新文件
             with open(new_file_path, 'w', encoding='utf-8') as f:
                 f.write(new_file_data)
+def cpdd_cli():
+    parser = argparse.ArgumentParser(
+        description='Copy Deep Designate - A tool for deep copying a project and designating a new project code.')
+
+    parser.add_argument('-d', '--char-mapping', type=str, required=True,
+                        help='A comma-separated list of target:source character mappings, e.g., "targetChars:sourceChars,targetChars:sourceChars"')
+
+    parser.add_argument('-o', '--output-path', type=str, required=True,
+                        help='The path to the output directory where the cloned project will be saved.')
+
+    parser.add_argument('source_path', type=str, nargs='?', default=None,
+                        help='The path to the source project. If not provided, assumes the current directory.')
+    args = parser.parse_args()
+
+
+    if args.source_path is None:
+       assert args.source_path is None, 'You must specify the source path'
+
+    char_mapping_list = args.char_mapping.split(',')
+    char_map = {}
+    for mapping in char_mapping_list:
+        target_chars, source_chars = mapping.split(':')
+        char_map[target_chars] = source_chars
+
+
+    rename_and_replace_in_file(args.source_path, args.output_path, char_map)
+
+
+def test():
+    # 使用函数
+    char_map = {'aaa': 'bbb', 'Aaa': 'Bbb'}
+    rename_and_replace_in_file(r'D:\tmp\sourceProject', r'D:\tmp\outputProject', char_map)
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # 使用函数
-    char_map = {'greatbay': 'gonline', 'Greatbay': 'Gonline'}
-    rename_and_replace_in_file(r'D:\tmp\greatbay-framework', r'D:\tmp\gonline-framework', char_map)
+    cpdd_cli()
+pass
 
 
